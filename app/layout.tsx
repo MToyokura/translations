d125912ec99216f69b346fa53ae6c035b494649c
MToyokura <mikitoyo6@gmail.com>
2025-11-10 20:39:18 +0900

    typescript

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,22 @@
-import { Footer, Layout, Navbar } from 'nextra-theme-docs'
-import 'nextra-theme-docs/style.css'
-import { Banner, Head } from 'nextra/components'
-import { getPageMap } from 'nextra/page-map'
- 
+import { Footer, Layout, Navbar } from "nextra-theme-docs";
+import "nextra-theme-docs/style.css";
+import { Head } from "nextra/components";
+import { getPageMap } from "nextra/page-map";
+
 export const metadata = {
   // Define your metadata here
   // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
-}
- 
-const banner = <Banner storageKey="some-key">Nextra 4.0 is released 🎉</Banner>
+};
+
+// const banner = <Banner storageKey="some-key">Nextra 4.0 is released 🎉</Banner>
 const navbar = (
   <Navbar
     logo={<b>Nextra</b>}
     // ... Your additional navbar options
   />
-)
-const footer = <Footer>MIT {new Date().getFullYear()} © Nextra.</Footer>
- 
+);
+const footer = <Footer>MIT {new Date().getFullYear()} © Nextra.</Footer>;
+
 export default async function RootLayout({ children }) {
   return (
     <html
@@ -34,7 +34,7 @@ export default async function RootLayout({ children }) {
       </Head>
       <body>
         <Layout
-          banner={banner}
+          // banner={banner}
           navbar={navbar}
           pageMap={await getPageMap()}
           docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
@@ -45,5 +45,5 @@ export default async function RootLayout({ children }) {
         </Layout>
       </body>
     </html>
-  )
+  );
 }
