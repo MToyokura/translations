43bbbeb20d9f1fc53319fb002936af2f0ed3b7ab
MToyokura <mikitoyo6@gmail.com>
2025-11-17 21:33:35 +0900

    open-access-and-reuse

---
+++ content/open-access-network/information/open-access-primers/_meta.ts
@@ -0,0 +1,17 @@
+import type { MetaRecord } from "nextra";
+
+/**
+ * type MetaRecordValue =
+ *  | TitleSchema
+ *  | PageItemSchema
+ *  | SeparatorSchema
+ *  | MenuSchema
+ *
+ * type MetaRecord = Record<string, MetaRecordValue>
+ **/
+const meta: MetaRecord = {
+  "what-does-open-access-mean": "",
+  "open-access-and-reuse": "",
+};
+
+export default meta;
